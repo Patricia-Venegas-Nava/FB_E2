8ca8c58563264b4a7064a4d42da54cd51e233ad7
Patricia-Venegas-Nava <L24550245@chihuahua2.tecnm.mx>
2024-11-04 15:35:30 -0600

    Add files via upload

---
+++ EVAL2_16_CICLOS_ARREGLOS/src/main/java/com/mycompany/eval2_16_ciclos_arreglos/EVAL2_16_CICLOS_ARREGLOS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval2_16_ciclos_arreglos;
+
+/**
+ *
+ * @author angel
+ */
+public class EVAL2_16_CICLOS_ARREGLOS {
+
+    public static void main(String[] args) {
+        int[] datos,x,y,z;
+                int val1,val2,arr[];
+                
+                arr = new int[10];
+                
+                for (int i =0; i<10; i++){
+                        arr[i] = (int) (Math.random()* 100);
+                }                        
+                      for(int i=0; i< 10; i++){
+                      System.out.println(arr[i]);
+    }
+    }
+}
