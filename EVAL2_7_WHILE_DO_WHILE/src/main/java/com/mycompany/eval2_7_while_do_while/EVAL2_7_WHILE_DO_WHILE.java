a88a02add1bb05157e82117e1a02a52d34ee0768
Patricia-Venegas-Nava <L24550245@chihuahua2.tecnm.mx>
2024-11-04 15:36:55 -0600

    Add files via upload

---
+++ EVAL2_7_WHILE_DO_WHILE/src/main/java/com/mycompany/eval2_7_while_do_while/EVAL2_7_WHILE_DO_WHILE.java
@@ -0,0 +1,35 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval2_7_while_do_while;
+
+/**
+ *
+ * @author angel
+ */
+public class EVAL2_7_WHILE_DO_WHILE {
+
+    public static void main(String[] args) {
+        //FOR-- REPITE LA CANTIDAD QUE SABEMOS DE REPETICIONES
+        /// WHILE CERO O MAS
+        //// DO WHILE UNA O MAS
+        
+        System.out.println("WHILE");
+        int val=1;
+        while(val<=10){
+            System.out.println(val+" - ");
+           val++;
+        }
+        val=1;
+          System.out.println("DO WHILE");
+    do{
+         
+       System.out.println(val+" - ");
+           val++;  
+    }while(val<=10);///hacer o repetir estast instruccines mientras eso sea verdad
+    
+    }
+}
+
+    
