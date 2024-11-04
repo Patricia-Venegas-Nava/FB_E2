ce643ed34a73b2bf00fac011ba7a5114b8492947
Patricia-Venegas-Nava <L24550245@chihuahua2.tecnm.mx>
2024-11-04 15:35:48 -0600

    Add files via upload

---
+++ EVAL2_17_ARREGLOS/src/main/java/com/mycompany/eval2_17_arreglos/EVAL2_17_ARREGLOS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval2_17_arreglos;
+
+/**
+ *
+ * @author angel
+ */
+public class EVAL2_17_ARREGLOS {
+
+    public static void main(String[] args) {
+       String diasem[]={"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
+       
+    int valores[]={20,50,100,200,500,10 };
+    for(int i=0; i < diasem.length; i++){
+    System.out.println(diasem[i]+ "-");
+}
+  
+     for(int i=0; i < valores.length; i++){
+    System.out.println(valores[i]+ "-");
+}
+   
+}
+}
