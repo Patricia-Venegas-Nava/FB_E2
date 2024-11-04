7c9d5bd869795b721e057a898f6c58a8d15b20bf
Patricia-Venegas-Nava <L24550245@chihuahua2.tecnm.mx>
2024-11-04 15:44:54 -0600

    Add files via upload

---
+++ EVAL2_14_BINARI/src/main/java/com/mycompany/eval2_14_binari/EVAL2_14_BINARI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval2_14_binari;
+
+/**
+ *
+ * @author angel
+ */
+public class EVAL2_14_BINARI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
